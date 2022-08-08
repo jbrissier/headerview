5ecfd5c8575b239557a2c94542d5c6fad7216758
Jochen Brissier <hello@jochenbrissier.de>
2022-08-08 16:49:29 +0200

    fead: show more info, time and ip

--- main.go
+++ main.go
@@ -4,16 +4,24 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 func headers(w http.ResponseWriter, req *http.Request) {
 	// This handler does something a little more sophisticated by reading all the HTTP request headers and echoing them into the response body.
 
+	fmt.Fprintf(w, "Hview\n\n\n")
+
+	fmt.Fprintf(w, "Time:\n-------------------\n")
+	fmt.Fprintf(w, "%s\n\n", time.Now().Format(time.RFC3339))
+	fmt.Fprintf(w, "Header:\n-------------------\n")
 	for name, headers := range req.Header {
 		for _, h := range headers {
 			fmt.Fprintf(w, "%v: %v\n", name, h)
 		}
 	}
+	fmt.Fprintf(w, "\nIP:\n-------------------\n")
+	fmt.Fprintf(w, "%s\n", req.RemoteAddr)
 }
 
 func main() {
